c4ce6d2c3eb399635cb9e783a59dda20f683a83e
Ming Lu <minglu@deloitte.com>
2019-04-20 22:06:10 -0400

    Working copy with application insight logs.

---
+++ ServiceSeed/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+namespace ServiceSeed
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            ILogger logger = null;
+            try
+            {
+                var host = CreateWebHostBuilder(args).Build();
+                logger = host.Services.GetRequiredService<ILogger<Program>>();
+                logger?.LogInformation("Service starting...");
+                await host.RunAsync();
+                logger?.LogInformation("Service ended.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                logger?.LogCritical(ex, "Service terminated unexpectedly.");
+            }
+            finally
+            {
+                await Task.Delay(10000);
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    config.SetBasePath(builderContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: false)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+                    configLogging.AddConsole((options) =>
+                    {
+                        options.IncludeScopes = Convert.ToBoolean(hostContext.Configuration["Logging:IncludeScopes"]);
+                    });
+                    configLogging.AddApplicationInsights(hostContext.Configuration["Logging:ApplicationInsights:Instrumentationkey"]);
+
+                    // Optional: Apply filters to configure LogLevel Trace or above is sent to
+                    // ApplicationInsights for all categories.
+                    configLogging.AddFilter<ApplicationInsightsLoggerProvider>("", LogLevel.Trace);
+
+                    // Additional filtering For category starting in "Microsoft",
+                    // only Warning or above will be sent to Application Insights.
+                    configLogging.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Warning);
+
+                })
+                .UseStartup<Startup>();
+    }
+}
---
+++ ServiceSeed/ScopedLoggingMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceSeed
+{
+    public class ScopedLoggingMiddleware
+    {
+        const string CORRELATION_ID_HEADER_NAME = "CorrelationID";
+        private readonly RequestDelegate next;
+        private readonly ILogger<ScopedLoggingMiddleware> logger;
+
+        public ScopedLoggingMiddleware(RequestDelegate next, ILogger<ScopedLoggingMiddleware> logger)
+        {
+            this.next = next ?? throw new System.ArgumentNullException(nameof(next));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context == null) throw new System.ArgumentNullException(nameof(context));
+
+            var correlationId = GetOrAddCorrelationHeader(context);
+
+            try
+            {
+                var loggerState = new LoggerState
+                {
+                    [CORRELATION_ID_HEADER_NAME] = correlationId
+                    //Add any number of properties to be logged under a single scope
+                };
+
+                using (logger.BeginScope(loggerState))
+                {
+                    await next(context);
+                }
+            }
+            //To make sure that we don't loose the scope in case of an unexpected error
+            catch (Exception ex) when (LogOnUnexpectedError(ex))
+            {
+            }
+        }
+
+        private string GetOrAddCorrelationHeader(HttpContext context)
+        {
+            if (context == null) throw new System.ArgumentNullException(nameof(context));
+
+            if (string.IsNullOrWhiteSpace(context.Request.Headers[CORRELATION_ID_HEADER_NAME]))
+                context.Request.Headers[CORRELATION_ID_HEADER_NAME] = Guid.NewGuid().ToString();
+
+            return context.Request.Headers[CORRELATION_ID_HEADER_NAME];
+        }
+
+        private bool LogOnUnexpectedError(Exception ex)
+        {
+            logger.LogError(ex, "An unexpected exception occured!");
+            return true;
+        }
+    }
+
+    internal class LoggerState : Dictionary<string, object>
+    {
+        const string seperator = " => ";
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach (var item in this)
+                builder.Append($"{item.Key}:{item.Value}{seperator}");
+
+            return builder.ToString().Replace(seperator, string.Empty);
+        }
+    }
+}
